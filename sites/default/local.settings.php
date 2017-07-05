6f84d0f77e5a94282d342af25caa9f55ff6d3a23
Kyle Taylor <kyletaylored@gmail.com>
2017-07-05 10:47:47 -0500

    Remove local.settings.php

--- sites/default/local.settings.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$databases['default']['default'] = array(
-    'driver' => 'mysql',
-    'database' => 'drupal',
-    'username' => 'drupal',
-    'password' => 'drupal',
-    'host' => 'localhost'
-);
